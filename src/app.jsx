6c73842dbcbf0ac9a78c98e0c89056e9116f7e23
cksdndbd95 <cksdndbd95@gmail.com>
2021-03-11 23:23:15 +0900

    react

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 import './app.css';
 
 function App() {
     const [videos, setVideos] = useState([]);
+
+    const search = (query) => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow',
+        };
+
+        fetch(
+            `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=24&q=${query}&type=videos&key=AIzaSyBpXbgoPSkptnIkOZJHPYWzn9L-yEUKuus`,
+            requestOptions,
+        )
+            .then((response) => response.json())
+            .then((result) =>
+                result.items.map((item) => ({ ...item, id: item.id.videoId })),
+            )
+            .then((items) => setVideos(items))
+            .catch((error) => console.log('error', error));
+    };
 
     useEffect(() => {
         const requestOptions = {
@@ -12,14 +31,20 @@ function App() {
         };
 
         fetch(
-            'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&key=AIzaSyBpXbgoPSkptnIkOZJHPYWzn9L-yEUKuus',
+            'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=24&key=AIzaSyBpXbgoPSkptnIkOZJHPYWzn9L-yEUKuus',
             requestOptions,
         )
             .then((response) => response.json())
             .then((result) => setVideos(result.items))
             .catch((error) => console.log('error', error));
     }, []);
-    return <VideoList videos={videos} />;
+
+    return (
+        <>
+            <SearchHeader search={search} />
+            <VideoList videos={videos} />
+        </>
+    );
 }
 
 export default App;
--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,5 +1,41 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 
-const SearchHeader = () => {};
+const SearchHeader = ({ search }) => {
+    const inputRef = useRef();
+
+    const handleSubmit = () => {
+        const value = inputRef.current.value;
+        search(value);
+    };
+
+    const onClick = () => {
+        handleSubmit();
+    };
+
+    const onSubmit = (event) => {
+        if (event.key === 'Enter') {
+            handleSubmit();
+        }
+    };
+
+    return (
+        <header>
+            <div className="search-logo">
+                <img src="images/logo.png" alt="search-logo" />
+                <span className="search-title">YouTube</span>
+            </div>
+            <div>
+                <input ref={inputRef} type="text" onKeyPress={onSubmit} />
+                <button onClick={onClick}>
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+            <div className="search-btn">
+                <i className="fas fa-video"></i>
+                <i className="fas fa-bell"></i>
+            </div>
+        </header>
+    );
+};
 
 export default SearchHeader;
